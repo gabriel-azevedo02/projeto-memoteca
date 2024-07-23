0a2f11a470f58d4a04edd048bf28921a0c7bca04
gabriel-azevedo02 <gabrielribeiroazevedo02@gmail.com>
2024-07-22 22:27:09 -0300

    adicionando eventos nos botões de salvar e cancelar.

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -21,4 +21,12 @@ export class CriarPensamentoComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  criarPensamento(){
+    alert("Novo pensamento criado");
+  }
+
+  cancelarPensamento(){
+    alert("Ação cancelada!");
+  }
+
 }
