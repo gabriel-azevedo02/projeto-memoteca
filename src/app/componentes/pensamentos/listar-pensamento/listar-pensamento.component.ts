ce85f31a3139033f4cf0b6e1a4596e9acdce96d3
gabriel-azevedo02 <gabrielribeiroazevedo02@gmail.com>
2024-07-28 22:51:55 -0300

    implemento as diretrizes do angular, ngIf, ngFor, ngClass

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
+  listaPensamentos = [
+    {
+      conteudo: 'Passo informações para o componente filho',
+      autoria: 'Componente pai',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'Minha propriedade é decorada com o @Input',
+      autoria: 'Componente filho',
+      modelo: 'modelo2'
+    },
+    {
+      conteudo: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates itaque quidem asperiores enim nesciunt id. Modi illo cupiditate, perspiciatis corporis possimus sunt ipsum! Voluptatibus, tenetur corrupti rem deserunt odit amet?odit ametodit ametodit ametodit ametodit ametodit ametodit ametodit amet',
+      autoria: '',
+      modelo: 'modelo1'
+    },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
